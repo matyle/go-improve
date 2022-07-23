1d96fc1fe757bdd8bd1fb3853b53dc48692d0127
MatyTan <matytan@bituniverse.org>
2022-07-23 10:54:23 +0800

    ch3

--- src/main.go
+++ src/main.go
@@ -1,20 +1,26 @@
 // Package main provides ...
 package main
 
-import "goimpove/part4/src/zuoer"
-
 func main() {
 	// zuoer.Init()
 	// zuoer.InitOptions()
 	// zuoer.Echo([]int{1, 2, 3, 4, 5})
-	nums := []int{1, 2, 3, 4, 5}
+	// nums := []int{1, 2, 3, 4, 5}
 	//非 pipeline 方法
 
 	//1+3+5
-	for v := range zuoer.Sum(zuoer.Odd(zuoer.Echo(nums))) {
-		println(v)
-	}
+	// for v := range zuoer.Sum(zuoer.Odd(zuoer.Echo(nums))) {
+	// 	println(v)
+	// }
 
-	//使用 pipelin
+	//使用 pipeline 方法
+
+	//
+	ch := make(chan int)
+	// go func() {
+	// 	ch <- ch3.NumToChannel()
+	// }()
+	ch <- 1
+	println(<-ch)
 
 }
